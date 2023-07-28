d8fca1f2a7a748296244b793b90cb042927a5e53
Bernhard Bablok <bablokb@gmx.de>
2023-07-28 10:35:24 +0200

    added support for Pimoroni's Tufty2040

---
+++ ports/raspberrypi/boards/pimoroni_tufty2040/mpconfigboard.h
@@ -0,0 +1,11 @@
+#define MICROPY_HW_BOARD_NAME "Pimoroni Tufty 2040"
+#define MICROPY_HW_MCU_NAME "rp2040"
+
+// Status LED
+#define MICROPY_HW_LED_STATUS (&pin_GPIO25)
+
+#define DEFAULT_UART_BUS_TX (&pin_GPIO0)
+#define DEFAULT_UART_BUS_RX (&pin_GPIO1)
+
+#define DEFAULT_I2C_BUS_SDA (&pin_GPIO4)
+#define DEFAULT_I2C_BUS_SCL (&pin_GPIO5)
